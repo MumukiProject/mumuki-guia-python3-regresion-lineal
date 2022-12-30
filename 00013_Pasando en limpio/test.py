77c4d265e8c5f1df7de387bd7f15ab5cff804eb3
franco@mumuki.org <franco@mumuki.org>
2022-12-30 02:39:20 +0000

    Mumuki Export on 2022-12-30 02:39:19 +0000

--- 00013_Pasando en limpio/test.py
+++ 00013_Pasando en limpio/test.py
@@ -1,10 +1,10 @@
 class Test(unittest.TestCase):
 
   def test_predecir_respuesta_20(self):
-    self.assertEquals(predecir_respuesta(20), 85.05642088787117, 0.5)
+    self.assertAlmostEqual(predecir_respuesta(20), 85.05642088787117, 5)
     
   def test_predecir_respuesta_30(self):
-    self.assertEquals(predecir_respuesta(30), 190.17295167042818, 0.5)
+    self.assertAlmostEqual(predecir_respuesta(30), 190.17295167042818, 5)
     
   def test_predecir_respuesta_15(self):
-    self.assertEquals(predecir_respuesta(15), 32.49815549659269, 0.5)    
+    self.assertAlmostEqual(predecir_respuesta(15), 32.49815549659269, 5)    
